57059b3651ff63f12865c41ae6b3b00262648fd8
Mats Liljegren <liljegren.mats@gmail.com>
2016-02-18 21:38:16 +0100

    send-email: Seems to work
    Sent 200-300 mails, and it seems like it worked well.
    
    Signed-off-by: Mats Liljegren <liljegren.mats@gmail.com>

--- send-email.sh
+++ send-email.sh
@@ -1,84 +1,152 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 
 # Copyright (C) Mats G. Liljegren <mats@mexit.se>
 # SPDX-License-Identifier: BSD-2-Clause
 
-dryRun=1
+dryRun=0
 
 # Read configuration file
 source ~/.gpg-signing/smtp-passwd.cfg
 
+# Working directory
+readonly wdir=$(mktemp -d -t send-email.XXXXXXXXXX)
+
+# Output directory
+readonly odir="$(pwd)/out"
+
+# Cleanup function run when script exits
+function finish {
+    rm -rf "$wdir"
+}
+      
+trap finish EXIT
+
+#
+# Check for needed dependencies
+#
+
 readonly GPG="$(which gpg2)"
 
-[[ -z "$GPG" ]] && {
-    printf "Could not find 'gpg2' in PATH, please install it."
+[[ -z "${GPG:-}" ]] && {
+    printf "Could not find 'gpg2' in PATH, please install it.\n"
     exit 1
 }
 
 readonly MIME_CONSTRUCT="$(which mime-construct)"
 
-[[ -z "$MIME_CONSTRUCT" ]] && {
-    printf "Could not find 'mime-construct' in PATH, please install it."
+[[ -z "${MIME_CONSTRUCT:-}" ]] && {
+    printf "Could not find 'mime-construct' in PATH, please install it.\n"
     exit 1
 }
 
 readonly SENDEMAIL="$(which sendemail)"
 
-[[ -z "$SENDEMAIL" ]] && {
-    printf "Could not find 'sendemail' in PATH, please install it."
+[[ -z "${SENDEMAIL:-}" ]] && {
+    printf "Could not find 'sendemail' in PATH, please install it.\n"
     exit 1
 }
 
+#
+# Print send-mail.template to stdout while replacing keywords with parameters
+# given
+#
+
 translateTemplate() {
     declare -r keyId="$1"
+    declare -r signeeUid="$2"
     declare -r signerName="$2"
 
     declare -r msgTemplate="$(dirname "$(readlink -f "$BASH_SOURCE")")/send-email.template"
 
-    cat $msgTemplate | sed 's/\${keyId}/'"${keyId}/" | sed 's/\${signerName}/'"${signerName}/"
+    cat $msgTemplate | sed 's/\${keyId}/'"${keyId}/g" | sed 's/\${signerName}/'"${signerName}/g" | sed 's/\${signeeUid}/'"${signeeUid}/g"
 }
 
+#
+# Create and send the mail
+#
+
 createMail() {
-    declare -r signeeEmail="$1"
+    declare -r signeeUid="$1"
     declare -r keyFile="$2"
     declare -r keyId="$3"
+
+    # Extract destination email address from signee uid
+    declare -r signeeEmail=$(echo "${signeeUid}" | sed -rn 's/.*<(.*)>/\1/p')
+
+    [[ -z "${signeeEmail:-}" ]] && {
+        printf "$signeeEmail: No e-mail address to send to, skipping\n"
+	return 0
+    }
 
     declare -r nl='
 '    
     declare -r enc_type=application/pgp-encrypted
     declare -r enc_params="Version: 1$nl"
 
-    declare -r mailBodyToEncrypt=$($MIME_CONSTRUCT --subpart --file <(translateTemplate "${keyId}" "${signerName}") --file-attach $keyFile)
+    # Create e-mail in layers, starting from the most inner part which consists
+    # message and the key to be sent, all packaged as a stand-alone mime text
+    declare -r mailBodyToEncrypt=$($MIME_CONSTRUCT --output --type 'text/plain; charset="utf-8"' --file <(translateTemplate "${keyId}" "${signeeUid}" "${signerName}") --type "application/pgp-keys" --attachment "$keyFile" --file $keyFile) 
 
-    declare -r mailBodyEncrypted=$(echo "$mailBodyToEncrypt" | $GPG --encrypt --armor -r "$signeeEmail")
+    [[ -z "${mailBodyToEncrypt:-}" ]] && return 1 
 
+    # Import key to our own keyring so we can use it for encryption
+    # It might be imported already if we're sending several messages for the same public key,
+    # so ignore errors.
+    $GPG --no-default-keyring --keyring ${wdir}/${keyId}.keyring --import ${keyFile} || true
+
+    # Encrypt the inner e-mail text with the public key
+    declare -r mailBodyEncrypted=$(echo "$mailBodyToEncrypt" | $GPG --no-default-keyring --keyring ${wdir}/${keyId}.keyring --trust-model always --encrypt --armor -r "$signeeEmail") 
+
+    [[ -z "${mailBodyEncrypted:-}" ]] && return 1
+
+    # Create an outer mime envelope for the actual message, consisting of
+    # non-encrypted headers and an encrypted body
     declare -r mailBody=$(echo "$mailBodyEncrypted" | $MIME_CONSTRUCT --output \
           --subject "Your signed PGP key $keyId" \
 	  --to "$signeeEmail" \
           --multipart "multipart/encrypted; protocol=\"$enc_type\"" \
           --type "$enc_type" \
+	  --attachment "signedkey.msg" \
+	  --encoding '7bit' \
 	  --string "$enc_params" \
-	  --file -)
+	  --attachment "msg.asc" \
+	  --type 'application/octet-stream; name="msg.asc"' \
+	  --part-header "Content-Disposition: inline; filename=\"msg.asc\"" \
+	  --file -) 
 
-    (( dryRun )) || echo "$mailBody" | $SENDEMAIL -q -l sendemail.log -o message-format=raw -s "$smtpServer" -t "$signeeEmail" -f "${smtpFrom}" -o username="$smtpUser" -o password="$smtpPasswd" -o tls="$smtpTls"
+    # Send e-mail unless dyRun is true
+    (( dryRun )) || echo "$mailBody" | $SENDEMAIL -q -l sendemail.log -o message-format=raw -s "$smtpServer" -t "$signeeEmail" -f "${smtpFrom}" -o username="$smtpUser" -o password="$smtpPasswd" -o tls="$smtpTls" || return 1
 
-    printf "${signeeEmail} ${keyId}: Mail sent to $signeeEmail"
+    printf "${signeeUid} ${keyId}: Mail sent to $signeeEmail"
     (( dryRun )) && printf " - Dry run"
     echo
 }
 
+#
+# Iterate key files found in the current directory and send mail for each of them
+#
+
 iterateFiles() {
     declare signeeKeyId
-    declare signeeEmail
+    declare signeeUid
     declare signerKeyId
+    declare file
  
     for file in *-signed-by-${signerFullKeyId}.asc; do
+	printf "--- File: ${file}\n"
+
+        # First part of file name is the key ID of signee key, extract it
 	read signeeKeyId signerKeyId < <(echo "$file" | sed -r 's/^([0-9A-Za-z]*)\.[0-9]+-signed-by-([0-9A-Za-z]*).asc$/\1 \2/')
-	signeeEmail="$($GPG --list-packets $file | egrep -m1 '^:user ID packet: ' | sed -rn 's/.*<(.*)>\"/\1/p')"
+
+	# Get signee UID from the key file
+	signeeUid="$($GPG --list-packets $file | egrep -m1 '^:user ID packet: ' | sed -rn 's/^:user ID packet: \"(.*)\"/\1/p')"
+
+	# Send the e-mail
+        createMail "${signeeUid}" "${file}" "${signeeKeyId}" || printf "WARNING: ${signeeUid} ${signeeKeyId}: Failed sending mail, ignoring\n"
     done
 }
 
-createMail mats@mexit.se 2209D6902F969C95.1-signed-by-CB9C8689AEA6A954.asc CB9C8689AEA6A954 CB9C8689AEA6A954
+iterateFiles
 
-#iterateFiles
+printf "\n---- Done ----\n\n"
 
