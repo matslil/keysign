69d61f1612d7355497ff1f03ba593a66c5da1c77
Mats Liljegren <liljegren.mats@gmail.com>
2016-02-21 20:06:32 +0100

    send-email.sh: Fixed illegal printf and wrong signer name
    When running the script one printf caused an illegal option error.
    The signer name also became wrong due to another bug, so the mail got
    signed with the name of the receiver rather than the sender.
    
    Signed-off-by: Mats Liljegren <liljegren.mats@gmail.com>

--- send-email.sh
+++ send-email.sh
@@ -46,21 +46,21 @@ trap finish EXIT
 readonly GPG="$(which gpg2)"
 
 [[ -z "${GPG:-}" ]] && {
-    printf "Could not find 'gpg2' in PATH, please install it.\n"
+    printf -- "Could not find 'gpg2' in PATH, please install it.\n"
     exit 1
 }
 
 readonly MIME_CONSTRUCT="$(which mime-construct)"
 
 [[ -z "${MIME_CONSTRUCT:-}" ]] && {
-    printf "Could not find 'mime-construct' in PATH, please install it.\n"
+    printf -- "Could not find 'mime-construct' in PATH, please install it.\n"
     exit 1
 }
 
 readonly SENDEMAIL="$(which sendemail)"
 
 [[ -z "${SENDEMAIL:-}" ]] && {
-    printf "Could not find 'sendemail' in PATH, please install it.\n"
+    printf -- "Could not find 'sendemail' in PATH, please install it.\n"
     exit 1
 }
 
@@ -68,11 +68,13 @@ readonly SENDEMAIL="$(which sendemail)"
 # Print send-mail.template to stdout while replacing keywords with parameters
 # given
 #
+# Globals:
+#   signerName - Defined by configuration file
+#
 
 translateTemplate() {
     declare -r keyId="$1"
     declare -r signeeUid="$2"
-    declare -r signerName="$2"
 
     declare -r msgTemplate="$(dirname "$(readlink -f "$BASH_SOURCE")")/send-email.template"
 
@@ -92,7 +94,7 @@ createMail() {
     declare -r signeeEmail=$(echo "${signeeUid}" | sed -rn 's/.*<(.*)>/\1/p')
 
     [[ -z "${signeeEmail:-}" ]] && {
-        printf "$signeeEmail: No e-mail address to send to, skipping\n"
+        printf -- "$signeeEmail: No e-mail address to send to, skipping\n"
 	return 0
     }
 
@@ -103,7 +105,7 @@ createMail() {
 
     # Create e-mail in layers, starting from the most inner part which consists
     # message and the key to be sent, all packaged as a stand-alone mime text
-    declare -r mailBodyToEncrypt=$($MIME_CONSTRUCT --output --type 'text/plain; charset="utf-8"' --file <(translateTemplate "${keyId}" "${signeeUid}" "${signerName}") --type "application/pgp-keys" --attachment "$keyFile" --file $keyFile) 
+    declare -r mailBodyToEncrypt=$($MIME_CONSTRUCT --output --type 'text/plain; charset="utf-8"' --file <(translateTemplate "${keyId}" "${signeeUid}") --type "application/pgp-keys" --attachment "$keyFile" --file $keyFile) 
 
     [[ -z "${mailBodyToEncrypt:-}" ]] && return 1 
 
@@ -135,8 +137,8 @@ createMail() {
     # Send e-mail unless dyRun is true
     (( dryRun )) || echo "$mailBody" | $SENDEMAIL -q -l sendemail.log -o message-format=raw -s "$smtpServer" -t "$signeeEmail" -f "${smtpFrom}" -o username="$smtpUser" -o password="$smtpPasswd" -o tls="$smtpTls" || return 1
 
-    printf "${signeeUid} ${keyId}: Mail sent to $signeeEmail"
-    (( dryRun )) && printf " - Dry run"
+    printf -- "${signeeUid} ${keyId}: Mail sent to $signeeEmail"
+    (( dryRun )) && printf -- " - Dry run"
     echo
 }
 
@@ -151,7 +153,7 @@ iterateFiles() {
     declare file
  
     for file in *-signed-by-${signerFullKeyId}.asc; do
-	printf "--- File: ${file}\n"
+	printf -- "--- File: ${file}\n"
 
         # First part of file name is the key ID of signee key, extract it
 	read signeeKeyId signerKeyId < <(echo "$file" | sed -r 's/^([0-9A-Za-z]*)\.[0-9]+-signed-by-([0-9A-Za-z]*).asc$/\1 \2/')
@@ -160,11 +162,11 @@ iterateFiles() {
 	signeeUid="$($GPG --list-packets $file | egrep -m1 '^:user ID packet: ' | sed -rn 's/^:user ID packet: \"(.*)\"/\1/p')"
 
 	# Send the e-mail
-        createMail "${signeeUid}" "${file}" "${signeeKeyId}" || printf "WARNING: ${signeeUid} ${signeeKeyId}: Failed sending mail, ignoring\n"
+        createMail "${signeeUid}" "${file}" "${signeeKeyId}" || printf -- "WARNING: ${signeeUid} ${signeeKeyId}: Failed sending mail, ignoring\n"
     done
 }
 
 iterateFiles
 
-printf "\n---- Done ----\n\n"
+printf -- "\n---- Done ----\n\n"
 
